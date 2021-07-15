0482725362fa70abea2a9b45eed204719f3a8afb
bwesterg <benton.westergaard@gmail.com>
2021-07-15 12:50:40 -0600

    Added user form and fetch to connect to backend.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 
 import './App.css';
+import CreateUserForm from './CreateUserForm';
+
 
 function App() {
   console.log('hello world')
   return (
     <div className="App">
-      
+      <h1>Auth App</h1>
+      <h2>Create User</h2>
+      <CreateUserForm />
     </div>
   );
 }
---
+++ src/CreateUserForm.js
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+
+
+function CreateUserForm() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        console.log('username', username);
+        console.log('password', password);
+
+        fetch('http://localhost:3000/users', {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ user: { username, password } })
+        })
+        .then(response => response.json())
+        .then(newUser => console.log(newUser))
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input 
+                type="text" 
+                name="username" 
+                placeholder="username"
+                value={username}
+                onChange={(event) => setUsername(event.target.value)}
+            />
+            <input 
+                type="password" 
+                name="password" 
+                placeholder="password"
+                value={password}
+                onChange={(event) => setPassword(event.target.value)}
+            />
+            <input type="submit" value="create user" />
+        </form>
+    );
+}
+
+export default CreateUserForm;
